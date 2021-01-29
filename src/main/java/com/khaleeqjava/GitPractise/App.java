4d13f7fb57a6d7e3a1a9d079342c500b0f8258e8
khaleeqjava <59270863+khaleeqjava@users.noreply.github.com>
2021-01-29 11:45:10 -0500

    Changed something

--- src/main/java/com/khaleeqjava/GitPractise/App.java
+++ src/main/java/com/khaleeqjava/GitPractise/App.java
@@ -7,6 +7,9 @@ public class App {
 		if (data.equals(Constant.DATA_TYPE)) {
 			System.out.println("Wellcome Khaleeq");
 		}
+	} public void chnage(){
+	System.out.println("new change to Hello World!");
+
 	}
 
 	public static void main(String[] args) {
